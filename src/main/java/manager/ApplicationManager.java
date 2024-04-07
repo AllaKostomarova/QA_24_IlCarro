bbc86701a1d3ef538f1b5116ad775a44c442199c
Alla Kostomarova <alla.kostomarova@mail.ru>
2024-04-07 17:34:45 +0300

    add logback

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -3,10 +3,13 @@ package manager;
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+    Logger logger = LoggerFactory.getLogger(ApplicationManager.class);
     WebDriver wd;
     @Getter
     HelperUser helperUser;
@@ -15,9 +18,11 @@ public class ApplicationManager {
 
     public void init(){
         wd = new ChromeDriver();
+        logger.info("All tests run in Google Browser");
         wd.manage().window().maximize();
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wd.navigate().to("https://ilcarro.web.app/");
+        logger.info("The Link --> "+wd.getCurrentUrl());
         helperUser = new HelperUser(wd);
         helperCar = new HelperCar(wd);
     }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,10 +3,16 @@ package tests;
 import manager.ApplicationManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.lang.reflect.Method;
+
 public class TestBase {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
     static ApplicationManager app = new ApplicationManager();
 
     @BeforeSuite
@@ -17,6 +23,11 @@ public class TestBase {
     @AfterSuite
     public void teatDown(){
         app.stop();
+    }
+
+    @BeforeMethod
+    public void startLogger(Method m){
+        logger.info("Name of method-->"+m.getName());
     }
 
 
